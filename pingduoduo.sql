9410ccb47c737f60e540422f1a8b5c284444fcac
tianwater <7701037@qq.com>
2020-05-14 11:58:50 +0800

    加上了数据库的sql语句

---
+++ pingduoduo.sql
@@ -0,0 +1,102 @@
+﻿# Host: localhost  (Version: 5.5.53)
+# Date: 2020-05-14 11:58:26
+# Generator: MySQL-Front 5.3  (Build 4.234)
+
+/*!40101 SET NAMES utf8 */;
+
+#
+# Structure for table "goodsinfo"
+#
+
+DROP TABLE IF EXISTS `goodsinfo`;
+CREATE TABLE `goodsinfo` (
+  `goodsId` varchar(10) NOT NULL,
+  `goodsName` varchar(100) DEFAULT NULL,
+  `typeId` char(3) NOT NULL,
+  `goodsPrice` float DEFAULT NULL,
+  `goodsCount` int(11) DEFAULT NULL,
+  `goodsDesc` varchar(100) DEFAULT NULL,
+  `goodsImg` varchar(100) DEFAULT NULL,
+  `beiyong1` varchar(100) DEFAULT NULL,
+  `beiyong2` varchar(100) DEFAULT NULL,
+  `beiyong3` varchar(100) DEFAULT NULL,
+  `beiyong4` varchar(100) DEFAULT NULL,
+  `beiyong5` varchar(100) DEFAULT NULL,
+  `beiyong6` varchar(100) DEFAULT NULL,
+  `beiyong7` varchar(100) DEFAULT NULL,
+  `beiyong8` varchar(100) DEFAULT NULL,
+  `beiyong9` varchar(100) DEFAULT NULL,
+  `beiyong10` varchar(100) DEFAULT NULL,
+  `beiyong11` varchar(100) DEFAULT NULL,
+  `beiyong12` varchar(100) DEFAULT NULL,
+  `beiyong13` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`goodsId`),
+  KEY `typeId` (`typeId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goodsinfo"
+#
+
+/*!40000 ALTER TABLE `goodsinfo` DISABLE KEYS */;
+INSERT INTO `goodsinfo` VALUES ('01001','铅笔','001',1.5,20,'这是铅笔','img/b1.jpg','','','','','','','','','','','','',''),('01002','钢笔','001',55,10,'这是钢笔','img/b1.jpg','','','','','','','','','','','','',''),('01003','毛笔','001',35,100,'这是毛笔','img/b3.jpg','','','','','','','','','','','','',''),('01004','签字笔','001',5,100,'这是签字笔','img/b5.jpg','','','','','','','','','','','','',''),('01005','蜡笔','001',15,35,'蜡笔','img/b4.jpg','','','','','','','','','','','','','');
+/*!40000 ALTER TABLE `goodsinfo` ENABLE KEYS */;
+
+#
+# Structure for table "goodstype"
+#
+
+DROP TABLE IF EXISTS `goodstype`;
+CREATE TABLE `goodstype` (
+  `typeid` char(3) NOT NULL,
+  `goodstype` varchar(20) NOT NULL,
+  PRIMARY KEY (`typeid`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "goodstype"
+#
+
+/*!40000 ALTER TABLE `goodstype` DISABLE KEYS */;
+INSERT INTO `goodstype` VALUES ('001','文具'),('002','家电');
+/*!40000 ALTER TABLE `goodstype` ENABLE KEYS */;
+
+#
+# Structure for table "shoppingcart"
+#
+
+DROP TABLE IF EXISTS `shoppingcart`;
+CREATE TABLE `shoppingcart` (
+  `vipName` varchar(10) DEFAULT NULL,
+  `goodsId` varchar(10) DEFAULT NULL,
+  `goodsCount` int(11) DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "shoppingcart"
+#
+
+/*!40000 ALTER TABLE `shoppingcart` DISABLE KEYS */;
+INSERT INTO `shoppingcart` VALUES ('宋超波','01001',7),('宋超波','01003',4);
+/*!40000 ALTER TABLE `shoppingcart` ENABLE KEYS */;
+
+#
+# Structure for table "vip"
+#
+
+DROP TABLE IF EXISTS `vip`;
+CREATE TABLE `vip` (
+  `username` varchar(20) NOT NULL,
+  `userpass` varchar(16) NOT NULL,
+  `sex` char(2) DEFAULT '女',
+  `age` int(11) DEFAULT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+#
+# Data for table "vip"
+#
+
+/*!40000 ALTER TABLE `vip` DISABLE KEYS */;
+INSERT INTO `vip` VALUES ('18991274260','123666','女',NULL),('18991274261','123666','女',NULL),('18991274262','111','女',NULL),('18991274266','123666','女',NULL),('18991274267','123666','女',NULL),('18991274268','123666','女',NULL),('18991274269','123666','女',NULL);
+/*!40000 ALTER TABLE `vip` ENABLE KEYS */;
